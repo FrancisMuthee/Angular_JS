86e61b64388695b934501680f837bc443f6fc258
0757377860 <francisnjaramba2@gmail.com>
2024-03-31 17:22:02 +0300

    Self rank

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeranComponent } from './leran/leran.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    NavbarComponent,
+    ActivitiesComponent,
+    FooterComponent,
+    
+    LeranComponent,
+    ContactsComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/leran/leran.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leran',
+  templateUrl: './leran.component.html',
+  styleUrls: ['./leran.component.css']
+})
+export class LeranComponent {
+
+}
